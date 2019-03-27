ab9e43be57975d886950e2d6c5d4618e9cf42088
Eduardo Bertin <edubertin85@gmail.com>
2019-03-27 02:17:14 -0300

    Primeira estrutura

---
+++ routes/index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'NodoOne' });
+});
+
+
+router.get('/sobre', function(req, res, next) {
+  res.render('sobre', { title: 'NodoOne - Sobre' });
+});
+
+
+module.exports = router;
